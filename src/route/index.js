26c7da8afabecc2385fe1bb46b2bcb069f519b81
yuriy05 <dilayyura@gmail.com>
2023-09-20 21:52:31 +0300

    scroll&nav

--- src/route/index.js
+++ src/route/index.js
@@ -4,10 +4,14 @@ const router = express.Router()
 
 // Підключіть файли роутів
 const test = require('./test')
+const nav = require('./nav')
+const scroll = require('./scroll')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/nav', nav)
+router.use('/scroll', scroll)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
